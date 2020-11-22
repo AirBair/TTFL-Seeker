6ab0abdd188d742e3772b43387da3d43466aa568
Florian LE GOFF <contact@florianlegoff.com>
2020-11-22 15:20:56 +0100

    Log NBA Data's synchronization in a dedicated channel

--- src/Command/SyncNbaBoxscoresCommand.php
+++ src/Command/SyncNbaBoxscoresCommand.php
@@ -42,7 +42,7 @@ class SyncNbaBoxscoresCommand extends Command
 
         $result = $this->nbaDataSynchronizer->synchronizeBoxscores($day);
 
-        $io->success($result['games'].' games boxscores & '.$result['activePlayers'].' active players have been synchronized.');
+        $io->success($result['games'].' games boxscores with '.$result['activePlayers'].' active players have been synchronized for the date of '.$day->format('d/m/Y').'.');
 
         return Command::SUCCESS;
     }
