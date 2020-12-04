45f2afc5b3b8a51bfade363e84b7fe3615d51e6d
Florian LE GOFF <contact@florianlegoff.com>
2020-12-04 20:30:55 +0100

    Add console command to synchronize boxscore per period instead of day by day

---
+++ src/Command/SyncNbaBoxscoresPerPeriodCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SyncNbaBoxscoresPerPeriodCommand extends Command
+{
+    protected static $defaultName = 'app:sync-nba-boxscores-per-period';
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Synchronize boxscores (teams & players stats) on a given period from NBA API to the local database.')
+            ->addArgument('beginningDay', InputArgument::REQUIRED, 'Beginning day of the games under the format Y-m-d')
+            ->addArgument('endingDay', InputArgument::REQUIRED, 'Ending day of the games under the format Y-m-d');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $currentDay = new \DateTime($input->getArgument('beginningDay'));
+        $endingDay = new \DateTime($input->getArgument('endingDay'));
+
+        while ($currentDay <= $endingDay) {
+            $this->getApplication()->find(SyncNbaBoxscoresCommand::getDefaultName())->run(new ArrayInput([
+                'day' => $currentDay->format('Y-m-d'),
+            ]), $output);
+            $currentDay->modify('+1 day');
+        }
+
+        return Command::SUCCESS;
+    }
+}
