a3180d4175f5e971706fa81f77dbf80a21df3a2d
Florian LE GOFF <contact@florianlegoff.com>
2020-12-12 19:55:41 +0100

    Create a pluggable ListFilter component & include it on the NbaPlayersList

--- assets/vue/models/api/NbaPlayer.ts
+++ assets/vue/models/api/NbaPlayer.ts
@@ -1,5 +1,7 @@
 import AbstractResource from './AbstractResource'
 import NbaTeam from './NbaTeam'
+import { ResourceCollectionFilter } from './ResourceCollection'
+import { ResourceFilterType } from './ResourceFilterType'
 
 export default class NbaPlayer extends AbstractResource {
     firstName?: string
@@ -7,10 +9,87 @@ export default class NbaPlayer extends AbstractResource {
     fullName?: string
     position?: string
     jersey?: number
-    isInjured?: string
+    isInjured?: boolean
     nbaTeam?: NbaTeam
     averageFantasyPoints?: number
     pastYearFantasyPoints?: number
     isAllowedInExoticLeague?: boolean
     updatedAt?: string
 }
+
+export class NbaPlayerFiltersParams {
+    firstName?: string
+    lastName?: string
+    fullName?: string
+    position?: string
+    jersey?: number
+    isInjured?: boolean
+    'nbaTeam.fullName'?: string
+    'averageFantasyPoints[gte]'?: number
+    'averageFantasyPoints[lte]'?: number
+    'pastYearFantasyPoints[gte]'?: number
+    'pastYearFantasyPoints[lte]'?: number
+    isAllowedInExoticLeague?: boolean
+}
+
+export const nbaPlayerAvailableFilters: ResourceCollectionFilter[] = [{
+    name: 'firstName',
+    label: 'First Name',
+    type: ResourceFilterType.String,
+    enable: false
+}, {
+    name: 'lastName',
+    label: 'Last Name',
+    type: ResourceFilterType.String,
+    enable: false
+}, {
+    name: 'fullName',
+    label: 'Full Name',
+    type: ResourceFilterType.String,
+    enable: false
+}, {
+    name: 'position',
+    label: 'Position',
+    type: ResourceFilterType.String,
+    enable: false
+}, {
+    name: 'jersey',
+    label: 'Jersey No.',
+    type: ResourceFilterType.String,
+    enable: false
+}, {
+    name: 'isInjured',
+    label: 'Injured ?',
+    type: ResourceFilterType.Boolean,
+    enable: false
+}, {
+    name: 'nbaTeam.fullName',
+    label: 'Nba Team',
+    type: ResourceFilterType.String,
+    enable: false
+}, {
+    name: 'averageFantasyPoints[gte]',
+    label: 'AVG Fantasy Points',
+    type: ResourceFilterType.Number,
+    enable: false
+}, {
+    name: 'averageFantasyPoints[gte]',
+    label: 'AVG Fantasy Points',
+    type: ResourceFilterType.Number,
+    enable: false
+}, {
+    name: 'pastYearFantasyPoints[gte]',
+    label: 'AVG Fantasy Points',
+    type: ResourceFilterType.Number,
+    enable: false
+}, {
+    name: 'pastYearFantasyPoints[gte]',
+    label: 'AVG Fantasy Points',
+    type: ResourceFilterType.Number,
+    enable: false
+}, {
+    name: 'isAllowedInExoticLeague',
+    label: 'Allowed in Exotic League ?',
+    type: ResourceFilterType.Boolean,
+    enable: false
+}]
--- assets/vue/models/api/ResourceCollection.ts
+++ assets/vue/models/api/ResourceCollection.ts
@@ -1,4 +1,5 @@
 import AbstractResource from './AbstractResource'
+import { ResourceFilterType } from './ResourceFilterType'
 
 export default class ResourceCollection<T extends AbstractResource> {
     'hydra:member': T[]
@@ -8,4 +9,14 @@ export default class ResourceCollection<T extends AbstractResource> {
         this['hydra:member'] = items
         this['hydra:totalItems'] = totalItems
     }
+}
+
+export interface ResourceCollectionFilter {
+    name: string;
+    label: string;
+    type: ResourceFilterType;
+    enable: boolean;
+    pickerMenu?: boolean;
+    value?: string | number | boolean;
+    choices?: { label: string, value: string | number }[];
 }
